8ff51df87d87900c3008fe1309159fdf0c461fff
Sheikh Riyadh <sheikhriyad350883@gmail.com>
2022-08-15 12:30:57 +0600

    added backgour color with backpack section which is tomato and added some border radius

--- js files/dom.js
+++ js files/dom.js
@@ -2,3 +2,8 @@ const headingH2 = document.getElementsByTagName('h2');
 for (const h2 of headingH2) {
     h2.style.color = 'lightblue';
 }
+
+const backpack = document.getElementById('backpack');
+backpack.style.backgroundColor = 'tomato';
+backpack.style.padding = '20px';
+backpack.style.borderRadius = '10px'
